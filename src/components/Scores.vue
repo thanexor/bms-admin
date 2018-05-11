1181cf290a749fe409e5235202f5f2ef8daaf787
thanexor <thanexor@gmail.com>
2018-05-10 23:23:33 -0500

    First stab at some styles for the scoreboard

--- src/components/Scores.vue
+++ src/components/Scores.vue
@@ -1,50 +1,98 @@
 <template>
     <section class="subsection">
         <h2>Scoreboard</h2>
-        <table>
+        <table class="scoreboard">
             <tr>
-                <td>Thane</td>
-                <td>3</td>
+                <th>Participant</th>
+                <th class="scoreboard__score scoreboard__score--head">Points</th>
             </tr>
             <tr>
-                <td>Jackson</td>
-                <td>3</td>
+                <td>
+                    <h4>Thane</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">3</td>
             </tr>
             <tr>
-                <td>Mr Poop </td>
-                <td>3</td>
+                <td>
+                    <h4>Jackson</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">3</td>
             </tr>
             <tr>
-                <td>Chris</td>
-                <td>3</td>
+                <td>
+                    <h4>Mr Poop</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">3</td>
             </tr>
             <tr>
-                <td>Larry</td>
-                <td>6</td>
+                <td>
+                    <h4>Chris</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">3</td>
             </tr>
             <tr>
-                <td>Jordan</td>
-                <td>2</td>
+                <td>
+                    <h4>Larry</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">6</td>
             </tr>
             <tr>
-                <td>Vanessa</td>
-                <td>4</td>
+                <td>
+                    <h4>Jordan</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">2</td>
             </tr>
             <tr>
-                <td>Megan</td>
-                <td>8</td>
+                <td>
+                    <h4>Vanessa</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">4</td>
             </tr>
             <tr>
-                <td>Wizzy</td>
-                <td>4</td>
+                <td>
+                    <h4>Megan</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">8</td>
             </tr>
             <tr>
-                <td>Katy</td>
-                <td>2</td>
+                <td>
+                    <h4>Wizzy</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">4</td>
             </tr>
             <tr>
-                <td>Beth</td>
-                <td>3</td>
+                <td>
+                    <h4>Katy</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">2</td>
+            </tr>
+            <tr>
+                <td>
+                    <h4>Beth</h4>
+                    <p>Has made 5 picks</p>
+                    <p>Last picked Road House on Apr 20, 2018</p>
+                </td>
+                <td class="scoreboard__score">3</td>
             </tr>
         </table>
     </section>
